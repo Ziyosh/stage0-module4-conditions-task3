6ac450f6189479a47032f570e91c745469ac8af5
Ziyosh <ziyodaakhmedova1998@gmail.com>
2022-10-20 19:27:53 +0300

    module4-task3 commit

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if ((character>='a'&&character<='z'))
+            System.out.println("Vowel");
+        else if ((character>='A'&&character<='Z'))
+            System.out.println("Consonant");
+        else
+            System.out.println("wrong alphabet!");
     }
 }
